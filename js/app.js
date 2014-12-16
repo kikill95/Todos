c42774df3b00d55e1393156061addf666f903dc4
kikill95 <kirill.gus@gmail.com>
2014-12-16 15:05:49 +0200

    Adding creating Todo

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
-//Deletee all selected
+//Create Todo
+function newLi() {
+    var list = document.getElementById('maden-todos');
+
+    var li = document.createElement('li'),
+        mark = document.createElement('input'),
+        check = document.createElement('input'),
+        p = document.createElement('p'),
+        btn = document.createElement('input');
+    mark.type = 'button';
+    mark.className = 'mark-done';
+    check.type = 'checkbox';
+    check.className = 'preparing-to-delete';
+    p.innerHTML = document.getElementById('creating').value;
+    mark.addEventListener('click', function (event) {
+        var target = event.target,
+            p = target.parentNode.getElementsByTagName('p')[0];
+        if (p.className !== 'marked') {
+            p.className = 'marked';
+        } else {
+            p.className = '';
+        }
+    });
+    btn.type = 'button';
+    btn.value = 'X';
+    btn.className = 'delete';
+    btn.addEventListener('click', function (event) {
+        var target = event.target,
+            parent = target.parentNode;
+        parent.parentNode.removeChild(parent);
+    });
+
+    li.appendChild(mark);
+    li.appendChild(check);
+    li.appendChild(p);
+    li.appendChild(btn);
+
+    list.appendChild(li);
+}
+
+var create = document.getElementById('new-li');
+create.addEventListener('click', newLi, false);
+
+//Delete all selected
 function deleteAllSelected() {
     var element = document.getElementById('maden-todos');
 
@@ -9,12 +52,3 @@ function deleteAllSelected() {
 
 var el = document.getElementById('deletion');
 el.addEventListener('click', deleteAllSelected, false);
-
-//Delete current
-[].forEach.call(document.querySelectorAll('.delete'), function(current) {
-    current.addEventListener('click', function (event) {
-        var target = event.target,
-            parent = target.parentNode;
-        parent.parentNode.removeChild(parent);
-    })
-});
