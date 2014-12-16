123ff7cdb94ce3fe7e7bda764a8883da07efd9f4
kikill95 <kirill.gus@gmail.com>
2014-12-16 13:16:57 +0200

    Delete current

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 //Deletee all selected
 function deleteAllSelected() {
-    var element = document.getElementById("maden-todos");
+    var element = document.getElementById('maden-todos');
 
-    while (document.querySelector(":checked") !== null) {
-        element.removeChild(document.querySelector(":checked").parentNode);
+    while (document.querySelector(':checked') !== null) {
+        element.removeChild(document.querySelector(':checked').parentNode);
     }
 }
 
-var el = document.getElementById("deletion");
-el.addEventListener("click", deleteAllSelected, false);
+var el = document.getElementById('deletion');
+el.addEventListener('click', deleteAllSelected, false);
+
+//Delete current
+[].forEach.call(document.querySelectorAll('.delete'), function(current) {
+    current.addEventListener('click', function (event) {
+        var target = event.target,
+            parent = target.parentNode;
+        parent.parentNode.removeChild(parent);
+    })
+});
