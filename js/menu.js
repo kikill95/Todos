17576be02e39313d4163264885b4b54822a657df
kikill95 <kirill.gus@gmail.com>
2015-01-11 23:17:43 +0200

    Adding Mark-all-as-done button

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,16 @@
-document.getElementById('clear-storage').addEventListener('click', function() {
-    localStorage.clear();
-    data = ({
-        id: [],
-        name: [],
-        todo: []
-    });
-    document.getElementById('maden-boards').innerHTML = '';
+document.getElementById('info').addEventListener('click', function() {
+    var info = 'The amount of all todos is: ';
+    if (document.querySelectorAll('.name-of-todo').length > 0) {
+        info += document.querySelectorAll('.name-of-todo').length;
+    } else {
+        info += 'none';
+    }
+    info += '\nThe amount of maden todos is: ';
+    if (document.querySelectorAll('.marked-todo').length) {
+        info += document.querySelectorAll('.marked-todo').length;
+    } else {
+        info += 'none';
+    }
+    alert(info);
 }, false);
+
